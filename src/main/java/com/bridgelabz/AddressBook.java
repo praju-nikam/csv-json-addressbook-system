3ebddab0427803af0472e857d06dbbc7830030e6
praju-nikam <sawanprajakta84@gmail.com>
2022-06-15 08:48:01 +0530

    Added: add multiple address book in system,each address book is uniqu

--- src/main/java/com/bridgelabz/AddressBook.java
+++ src/main/java/com/bridgelabz/AddressBook.java
@@ -1,14 +1,17 @@
 package com.bridgelabz;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class AddressBook
 {
-        ArrayList<Contact> contactList = new ArrayList<>();
+       ArrayList<Contact> contactList = new ArrayList<>();
        static Scanner scanner = new Scanner(System.in);
+       static HashMap<String,ArrayList<Contact>> hashMap = new HashMap<>();
+       static AddressBook addressBook = new AddressBook();
 
-        // Added Contact by using contact object by getting input from user
+        // This method is used to add details to address book
         public void addDetail()
         {
                 Contact contact = new Contact();
@@ -118,6 +121,130 @@ public class AddressBook
                         {
                                 System.out.println("Enter Valid FirstName : ");
                         }
+                }
+        }
+
+        // Create address book
+        public void createAddressBook()
+        {
+                while(true)
+                {
+                  System.out.println("Enter your Choice : \n 1.Create new AddressBook \n 2.Edit Existing AddressBook \n " +
+                          "3.Display All AddressBook \n 4.Exit");
+                  int choice = scanner.nextInt();
+                  // if you choose option 4 then exist in process
+                  if(choice == 4)
+                  {
+                          System.out.println("Exited");
+                          break;
+                  }
+                  switch (choice)
+                  {
+                          case 1:
+                                  System.out.println("Enter name of AddressBook : ");
+                                  String addressBook_name = scanner.next();
+                                  // Condition to check Uniqueness of address book
+                                  if (hashMap.containsKey(addressBook_name))
+                                  {
+                                     System.out.println("Address Book Name Exits enter different name : ");
+                                     break;
+                                  }
+                                //  create array list object name as new_address_book
+                                  ArrayList<Contact> newAddressBook = new ArrayList<>();
+                                  contactList = newAddressBook;
+                                  while (true)
+                                  {
+                                    System.out.println("Enter your Choice : \n 1.Add Detail \n 2.Display Detail \n 3.Edit Detail \n" +
+                                                  " 4.Delete Detail \n 5.Exit ");
+                                    int choose = scanner.nextInt();
+                                    if (choose == 5)
+                                    {
+                                            System.out.println("Exited");
+                                            break;
+                                    }
+                                    switch (choose) {
+
+                                                  case 1:
+                                                          addressBook.addDetail();
+                                                          break;
+                                                  case 2:
+                                                          addressBook.display();
+                                                          break;
+                                                  case 3:
+                                                          addressBook.editDetails();
+                                                          break;
+                                                  case 4:
+                                                          addressBook.deleteDetails();
+                                                          break;
+                                                  case 5:
+                                                          System.out.println("You are Exit ");
+                                                  default:
+                                                          System.out.println(" You Enter Wrong Option");
+
+                                          }
+                                           hashMap.put(addressBook_name,contactList);
+                                          System.out.println(hashMap);
+                                          break;
+                                  }
+                          case 2:
+                                  System.out.println("Enter the name of AddressBook : ");
+                                  String addressBook_name_old =scanner.next();
+                                  // condition to check whether address book exists or no.
+                                  if (hashMap.containsKey(addressBook_name_old))
+                                  {
+                                          // Create object of  ArrayList<Contact>
+                                          ArrayList<Contact> old_AddressBook = new ArrayList<>();
+                                          // Store All data in old_AddressBook
+                                          contactList = old_AddressBook;
+                                          contactList = hashMap.get(addressBook_name_old);
+
+                                          while (true)
+                                          {
+                                                  System.out.println("Enter your Choice : \n 1.Add Detail \n 2.Display Detail \n" +
+                                                          " 3.Edit Detail \n 4.Delete Detail \n 5.Exit ");
+                                                  int choose = scanner.nextInt();
+                                                  if (choose == 5)
+                                                  {
+                                                          System.out.println("Exited");
+                                                          break;
+                                                  }
+                                                  switch (choose) {
+
+                                                          case 1:
+                                                                  addressBook.addDetail();
+                                                                  break;
+                                                          case 2:
+                                                                  addressBook.display();
+                                                                  break;
+                                                          case 3:
+                                                                  addressBook.editDetails();
+                                                                  break;
+                                                          case 4:
+                                                                  addressBook.deleteDetails();
+                                                                  break;
+                                                          case 5:
+                                                                  System.out.println("You are Exit ");
+                                                          default:
+                                                                  System.out.println(" You Enter Wrong Option");
+
+                                                  }
+                                                  hashMap.put(addressBook_name_old,contactList);
+                                                  System.out.println(hashMap);
+                                                  break;
+                                          }
+                                  }
+                                  else
+                                  {
+                                          System.out.println("Enter valid name of AddressBook : ");
+                                  }
+                          case 3:
+                                  System.out.println(hashMap);
+                                  break;
+
+                          default:
+                                  System.out.println("Enter valid option");
+                  }
+
                 }
         }
 
--- src/main/java/com/bridgelabz/AddressBookMain.java
+++ src/main/java/com/bridgelabz/AddressBookMain.java
@@ -10,33 +10,35 @@ public class AddressBookMain
         System.out.println();
         AddressBook addressBook = new AddressBook();
         Scanner scanner = new Scanner(System.in);
+//
+//        int choice;
+//        do {
+//            System.out.println("Enter your Choice : \n 1.Add Detail \n 2.Display Detail \n 3.Edit Detail \n" +
+//                    " 4.Delete Detail \n 5.Exit ");
+//            choice = scanner.nextInt();
+//            switch (choice) {
+//                case 1:
+//                    addressBook.addDetail();
+//                    break;
+//                case 2:
+//                    addressBook.display();
+//                    break;
+//                case 3:
+//                    addressBook.editDetails();
+//                    break;
+//                case 4:
+//                    addressBook.deleteDetails();
+//                    break;
+//                case 5:
+//                    System.out.println("You are Exit ");
+//                default:
+//                    System.out.println(" You Enter Wrong Option");
+//
+//            }
+//        }
+//        while (choice != 4);
 
-        int choice;
-        do {
-            System.out.println("Enter your Choice : \n 1.Add Detail \n 2.Display Detail \n 3.Edit Detail \n" +
-                    " 4.Delete Detail \n 5.Exit ");
-            choice = scanner.nextInt();
-            switch (choice) {
-                case 1:
-                    addressBook.addDetail();
-                    break;
-                case 2:
-                    addressBook.display();
-                    break;
-                case 3:
-                    addressBook.editDetails();
-                    break;
-                case 4:
-                    addressBook.deleteDetails();
-                    break;
-                case 5:
-                    System.out.println("You are Exit ");
-                default:
-                    System.out.println(" You Enter Wrong Option");
-
-            }
-        }
-        while (choice != 4);
+        addressBook.createAddressBook();
 
     }
 }
